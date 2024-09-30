ad271ea705e703db2d6e1838aa363c5817b4cfc0
nathy <nathalia.cericatto@gmail.com>
2024-09-30 17:33:28 -0300

    feat: fetchAdsData

---
+++ fetchDataAds.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function fetchDataAds() {
+    const apiKey = process.env.WINDSOR_API_KEY
+    const url = `https://connectors.windsor.ai/facebook?api_key=${apiKey}&date_from=2023-01-01&date_to=${new Date().toISOString().slice(0, 10)}&fields=campaign,clicks,spend,impressions,date`;
+
+    try {
+        const response = await axios.get(url)
+        console.log(response.data);
+        return response.data
+    } catch (error) {
+        console.error('Error fetching data from Windsor.ai:', error); 
+
+    }
+
+}
+
+fetchDataAds()
